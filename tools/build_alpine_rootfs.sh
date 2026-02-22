fd2b095e4e7eb4b7e2bf7a0388328fa0adf4299e
maceip <maceip@users.noreply.github.com>
2026-02-22 06:25:33 +0000

    fix(tools): quote CID variable to satisfy shellcheck

--- tools/build_alpine_rootfs.sh
+++ tools/build_alpine_rootfs.sh
@@ -8,10 +8,10 @@ docker build --network host --platform linux/riscv64 -t friscy-alpine-opt -f too
 CID=$(docker create friscy-alpine-opt)
 
 # Export the filesystem as a tarball to both locations
-docker export $CID > docs_site/rootfs.tar
+docker export "$CID" > docs_site/rootfs.tar
 cp docs_site/rootfs.tar friscy-bundle/rootfs.tar
 
 # Clean up the container
-docker rm $CID
+docker rm "$CID"
 
 echo "Successfully built and exported optimized Alpine rootfs."
